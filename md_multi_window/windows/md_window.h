344a4e25f870688e01895cfad50695bc3a7ad5d6
magicdvd <davidhyz@gmail.com>
2025-04-02 17:35:13 +0800

    add: more classes stable

---
+++ md_multi_window/windows/md_window.h
@@ -0,0 +1,61 @@
+#ifndef MD_MULTI_WINDOW_PLUGIN_MD_WINDOW_H_
+#define MD_MULTI_WINDOW_PLUGIN_MD_WINDOW_H_
+
+#include <windows.h>
+
+#include <flutter/flutter_view_controller.h>
+
+#include <cstdint>
+#include <memory>
+#include <string>
+
+namespace md_multi_window {
+
+class MdWindowCallback {
+
+  public:
+    virtual void OnWindowClose(std::string id) = 0;
+  
+    virtual void OnWindowDestroy(std::string id) = 0;
+  
+  };
+
+class MdWindow {
+
+ public:
+
+  MdWindow(std::string id, std::string args, const std::shared_ptr<MdWindowCallback> &callback);
+  MdWindow(std::string id, HWND handle, const std::shared_ptr<MdWindowCallback> &callback);
+  ~MdWindow();
+
+ protected:
+
+  HWND GetWindowHandle() { return window_handle_; };
+
+ private:
+
+  std::weak_ptr<MdWindowCallback> callback_;
+
+  std::string id_;
+
+  HWND window_handle_;
+
+  // The Flutter instance hosted by this window.
+  std::unique_ptr<flutter::FlutterViewController> flutter_controller_;
+
+  double scale_factor_;
+
+  bool destroyed_ = false;
+
+  //static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+  //static MdWindow *GetThisFromHandle(HWND window) noexcept;
+
+  //LRESULT MessageHandler(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
+
+  void Destroy();
+};
+
+}  // namespace md_multi_window
+
+#endif //MD_MULTI_WINDOW_PLUGIN_MD_WINDOW_H_
---
+++ md_multi_window/windows/md_window_manager.h
@@ -0,0 +1,33 @@
+#ifndef MD_MULTI_WINDOW_PLUGIN_MD_WINDOW_MANAGER_H_
+#define MD_MULTI_WINDOW_PLUGIN_MD_WINDOW_MANAGER_H_
+
+#include <cstdint>
+#include <string>
+#include <map>
+#include "md_window.h"
+
+
+namespace md_multi_window {
+
+class MdWindowManager : public std::enable_shared_from_this<MdWindowManager>, public MdWindowCallback{
+
+ public:
+  static MdWindowManager *Instance();
+
+  MdWindowManager();
+
+  std::string Create(std::string id, std::string args);
+  // void AddWindowAndNotifyAll(std::string id, HWND window_handle);
+
+  // flutter::EncodableList GetAllWindowIDs();
+  void OnWindowClose(std::string id) override;
+  void OnWindowDestroy(std::string id) override;
+ private:
+
+  std::map<std::string, std::unique_ptr<MdWindow>> windows_;
+
+};
+
+}  // namespace md_multi_window
+
+#endif //MD_MULTI_WINDOW_PLUGIN_MD_WINDOW_MANAGER_H_
